64513db67e1d302daffc92a75d1923af092e55e4
Henning Schaper <henningschaper@gmx.de>
2022-12-15 11:45:09 +0100

    Update config_getjudo.py

--- python/config_getjudo.py
+++ python/config_getjudo.py
@@ -1,1 +1,14 @@
-
+#-----------Config---------
+TOKEN = "1a2b3c4d5e6f1a2b3c4d5e6f1a2b3c4d"
+SERIAL = "1a2b3c4d5e6f"
+BROKER = "192.168.1.2"
+MQTTUSER = "my_mqttuser"
+MQTTPASSWD = "my_mqttpasswd"
+PORT = 1883
+LOCATION = "my_location"
+NAME = "Judo_isoftsaveplus"
+MANUFACTURER = "ShapeLabs.de"
+SW_VERSION = "1.2"
+STATE_UPDATE_INTERVAL = 20 #sec
+AVAILABILITY_ONLINE = "online"
+#------------------------
