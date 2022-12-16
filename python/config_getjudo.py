0314aa0121c50f38110be1300be53ec5253bf679
Henning Schaper <henningschaper@gmx.de>
2022-12-16 16:15:23 +0100

    Update config_getjudo.py

--- python/config_getjudo.py
+++ python/config_getjudo.py
@@ -1,14 +1,26 @@
-#-----------Config---------
-TOKEN = "1a2b3c4d5e6f1a2b3c4d5e6f1a2b3c4d"
-SERIAL = "1a2b3c4d5e6f"
-BROKER = "192.168.1.2"
-MQTTUSER = "my_mqttuser"
-MQTTPASSWD = "my_mqttpasswd"
-PORT = 1883
-LOCATION = "my_location"
-NAME = "Judo_isoftsaveplus"
-MANUFACTURER = "ShapeLabs.de"
+#!/urs/bin/python3
+# -*- coding: utf-8 -*-
+
+#Judo Config
+TOKEN = "1a2b3c4d5e6f1a2b3c4d5e6f1a2b3c4d"		#Just take the knmtoken from URL
+SERIAL = "1a2b3c4d5e6f"						            #Serial Number of Judo device
+
+#MQTT Config
+BROKER = "192.168.1.2"			    	#Broker IP
+USE_MQTT_AUTH = True				      #Set true, if user/pw authentification on broker, set false, if using anonymous login
+MQTTUSER = "my_mqttuser"		    	#only required if USE_MQTT_AUTH = True
+MQTTPASSWD = "my_mqttpasswd"    	#only required if USE_MQTT_AUTH = True
+PORT = 1883										    #MQTT PORT, 1883 default standard
+
+
+#General Config
+LOCATION = "my_location"					#Location of Judo device
+NAME = "Judo_isoftsaveplus"				#Name of Judo device
+MANUFACTURER = "ShapeLabs.de"			#CC BY-NC-SA 4.0
 SW_VERSION = "1.2"
-STATE_UPDATE_INTERVAL = 20 #sec
+STATE_UPDATE_INTERVAL = 20 				#Update interval in seconds
 AVAILABILITY_ONLINE = "online"
-#------------------------
+
+
+
+
