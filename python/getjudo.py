2729abc48001ac4854cca5edc227de94ea8a54ef
Henning Schaper <henningschaper@gmx.de>
2023-01-04 09:06:46 +0100

    Update getjudo.py
    - Authentification via user/pass instead of token
    - response error handling added
    - helper function fpr little endian hexstring to int added

--- python/getjudo.py
+++ python/getjudo.py
@@ -6,6 +6,8 @@ import json
 import time
 import gc
 import config_getjudo
+import hashlib
+import sys
 from paho.mqtt import client as mqtt
 
 class entity():
@@ -153,15 +155,33 @@ def start_regeneration():
 
 
 def send_command(index, data):
-    cmd_response = http.request('GET', f"https://www.myjudo.eu/interface/?token={config_getjudo.TOKEN}&group=register&command=write%20data&serial_number={my_serial}&dt={my_dt}&index={index}&data={data}&da={my_da}&role=customer")
+    cmd_response = http.request('GET', f"https://www.myjudo.eu/interface/?token={my_token}&group=register&command=write%20data&serial_number={my_serial}&dt={my_dt}&index={index}&data={data}&da={my_da}&role=customer")
     cmd_response_json = json.loads(cmd_response.data)
     if "status" in cmd_response_json:
         if cmd_response_json["status"] == "ok":
             return True
     return False
 
+
 def clamp(n, minn, maxn):
     return max(min(maxn, n), minn)
+
+
+def le_hex_to_int(hexstring):
+    # convert little endian hex to integer
+    return int.from_bytes(bytes.fromhex(hexstring), byteorder='little')
+
+
+def judo_login(username, password):
+    pwmd5 = hashlib.md5(password.encode("utf-8")).hexdigest()
+    login_response = http.request('GET', f"https://www.myjudo.eu/interface/?group=register&command=login&name=login&user={username}&password={pwmd5}&nohash=Service&role=customer")
+    login_response_json = json.loads(login_response.data)
+ 
+    if "token" in login_response_json:
+        print(f"Login successfull, got new token: {login_response_json['token']}")
+        return login_response_json['token']
+    else:
+        sys.exit("Login to myjudo.eu failed.")
 
 	
 #INIT
@@ -171,6 +191,8 @@ availability_topic = f"{config_getjudo.LOCATION}/{config_getjudo.NAME}/status"
 client_id = f"{config_getjudo.NAME}-{config_getjudo.LOCATION}"
 
 http = urllib3.PoolManager()
+
+my_token = judo_login(config_getjudo.JUDO_USER, config_getjudo.JUDO_PASSWORD)
 
 next_revision = entity("Revision_in", "mdi:account-wrench", "sensor", "Tagen")
 total_water = entity("Gesamtwasserverbrauch", "mdi:water", "total_increasing", "m³")
@@ -185,6 +207,7 @@ regenerations = entity("Anzahl_Regenerationen", "mdi:recycle-variant", "sensor")
 water_lock = entity("Leckageschutz", "mdi:pipe-valve", "switch")
 regeneration_start = entity("Regeneration", "mdi:recycle-variant", "switch")
 
+
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
@@ -194,66 +217,82 @@ client.will_set(availability_topic, config_getjudo.AVAILABILITY_OFFLINE, qos=0, 
 client.connect(config_getjudo.BROKER, config_getjudo.PORT, 60)
 client.loop_start()
 
+
 #MAIN LOOP
 while True:
     print("Sending GET to Cloud-Service...")
-    response = http.request('GET',f"https://www.myjudo.eu/interface/?token={config_getjudo.TOKEN}&group=register&command=get%20device%20data")
+    response = http.request('GET',f"https://www.myjudo.eu/interface/?token={my_token}&group=register&command=get%20device%20data")
     response_json = json.loads(response.data)
+    
+    if "status" in response_json:
+        if response_json["status"] ==  "ok":
+            print("Parsing values from response...")
+            my_serial = response_json["data"][0]["serialnumber"]
+            my_da = response_json["data"][0]["data"][0]["da"]
+            my_dt = response_json["data"][0]["data"][0]["dt"]
 
-    print("Parsing values from response...")
-    my_serial = response_json["data"][0]["serialnumber"]
-    my_da = response_json["data"][0]["data"][0]["da"]
-    my_dt = response_json["data"][0]["data"][0]["dt"]
+            #Next revision in days #7
+            val = response_json["data"][0]["data"][0]["data"]["7"]["data"]
+            next_revision.value = int(le_hex_to_int(val[0:4])/24)
 
-    #Next revision in days #7
-    val = response_json["data"][0]["data"][0]["data"]["7"]["data"]
-    next_revision.value = int(int(val[2:4] + val[0:2],16)/24)
+            #Total Water Consumption #8
+            val = response_json["data"][0]["data"][0]["data"]["8"]["data"]
+            total_water.value = float(le_hex_to_int(val[0:8]))/1000
 
-    #Total Water Consumption #8
-    val = response_json["data"][0]["data"][0]["data"]["8"]["data"]
-    total_water.value = int((val[6:8] + val[4:6] + val[2:4] + val[0:2]),16)
-    total_water.value = float(total_water.value)/1000
+            #Total Softwater Consumption #9
+            val = response_json["data"][0]["data"][0]["data"]["9"]["data"]
+            total_softwater.value = float(le_hex_to_int(val[0:8]))/1000
 
-    #Total Softwater Consumption #9
-    val = response_json["data"][0]["data"][0]["data"]["9"]["data"]
-    total_softwater.value = int((val[6:8] + val[4:6] + val[2:4] + val[0:2]),16)
-    total_softwater.value = float(total_softwater.value)/1000
 
-    #Salt Stock & Salt Range #94
-    val = response_json["data"][0]["data"][0]["data"]["94"]["data"]
-    salt_stock.value = int((val[2:4] + val[0:2]),16)/1000
-    salt_range.value = int((val[6:8] + val[4:6]),16)
+            #Salt Stock & Salt Range #94
+            val = response_json["data"][0]["data"][0]["data"]["94"]["data"]
+            salt_stock.value = le_hex_to_int(val[0:4])/1000
+            salt_range.value = le_hex_to_int(val[4:8])
 
-    #Input/Output Hardness, Water flow rate #790
-    val =response_json["data"][0]["data"][0]["data"]["790"]["data"] 
-    output_hardness.value = int(val[18:20],16)
-    input_hardness.value = int(val[54:56],16)
-    input_hardness.value = float(input_hardness.value)/2 + 2
-    water_flow.value = int((val[36:38] + val[34:36]),16)
+            #Input/Output Hardness, Water flow rate #790
+            val =response_json["data"][0]["data"][0]["data"]["790"]["data"] 
+            output_hardness.value = int(val[18:20],16)
+            input_hardness.value = int(val[54:56],16)
+            input_hardness.value = float(input_hardness.value)/2 + 2
+            water_flow.value = le_hex_to_int(val[34:38])
 
-    #Regnerations #791
-    val = response_json["data"][0]["data"][0]["data"]["791"]["data"]
-    regenerations.value = int((val[64:66] + val[62:64]),16)
-    regeneration_start.value = int(val[3:4])
-    if regeneration_start.value > 0:
-        regeneration_start.value = 1
+            #Regnerations #791
+            val = response_json["data"][0]["data"][0]["data"]["791"]["data"]
+            regenerations.value = le_hex_to_int(val[62:66])
+            regeneration_start.value = int(val[3:4])
+            if regeneration_start.value > 0:
+                regeneration_start.value = 1
 
-    #Battery capacity #93
-    val = response_json["data"][0]["data"][0]["data"]["93"]["data"]
-    batt_capacity.value = int(val[6:8],16)
+            #Battery capacity #93
+            val = response_json["data"][0]["data"][0]["data"]["93"]["data"]
+            batt_capacity.value = int(val[6:8],16)
 
-    #Leakage protection valve #792
-    val = response_json["data"][0]["data"][0]["data"]["792"]["data"]
-    water_lock.value = int(val[2:4],16)
-    if water_lock.value > 1:
-        water_lock.value = 1
+            #Leakage protection valve #792
+            val = response_json["data"][0]["data"][0]["data"]["792"]["data"]
+            water_lock.value = int(val[2:4],16)
+            if water_lock.value > 1:
+                water_lock.value = 1
 
-    print("Publishing parsed values over MQTT....")
-    outp_val_dict = {}
-    for obj in gc.get_objects():
-        if isinstance(obj, entity):
-            outp_val_dict[obj.name] = str(obj.value)
-    publish_json(client, state_topic, outp_val_dict)
+            print("Publishing parsed values over MQTT....")
+            client.publish(availability_topic, config_getjudo.AVAILABILITY_ONLINE, qos=0, retain=True)
+
+            outp_val_dict = {}
+            for obj in gc.get_objects():
+                if isinstance(obj, entity):
+                    outp_val_dict[obj.name] = str(obj.value)
+            #publish_json(client, state_topic, outp_val_dict)
+
+        elif response_json["status"] == "error":
+            if response_json["data"] == "login failed":
+                print("Error: No valid Token, trying to get a new one...")
+                my_token = judo_login(config_getjudo.JUDO_USER, config_getjudo.JUDO_PASSWORD)
+            else:
+                val = response_json["data"]
+                print(f"Response Error: {val}")
+        else:
+            sys.exit("Error: Unspecific response status")
+    else:
+        sys.exit("Error: Invalid response")
 
     print("Spend some time....")
     time.sleep(config_getjudo.STATE_UPDATE_INTERVAL)
