861b0e5303c3167d5c6944fa1a393d7672665d87
22k7H8zk <danielegger1@gmail.com>
2023-02-05 09:13:51 +0100

    Added HA Appdaemon files

---
+++ python/main_entity.py
@@ -0,0 +1,24 @@
+import time
+import sys
+import appdaemon.plugins.hass.hassapi as hass
+import config_getjudo
+#import config_getbayrol
+
+class main_loop(hass.Hass):
+    def initialize(self):
+        if config_getjudo.appdaemon:
+            import getjudo
+        #if config_getbayrol.appdaemon:
+        #    import getbayrol
+        while True:
+            #print("loop started")
+            try: 
+                if config_getjudo.appdaemon:
+                    self.run_in(getjudo.loop, config_getjudo.STATE_UPDATE_INTERVAL)
+                #if config_getbayrol.appdaemon:
+                #    self.run_in(getbayrol.loop, config_getbayrol.STATE_UPDATE_INTERVAL)
+            except Exception as e:
+                    crash = ["Error on line {}".format(sys.exc_info()[-1].tb_lineno),"\n",e]
+                    print(crash)  
+            #print("loop running")
+            time.sleep(config_getjudo.STATE_UPDATE_INTERVAL)              
