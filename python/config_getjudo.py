ba9c2be32caacb2331d64ae0fe221870f77691d8
Henning Schaper <henningschaper@gmx.de>
2023-02-08 12:24:25 +0100

    Update config_getjudo.py
    Limitations for sliders

--- python/config_getjudo.py
+++ python/config_getjudo.py
@@ -17,18 +17,26 @@ PORT = 1883                                     #MQTT PORT, 1883 default standar
 LOCATION = "my_location"                        #Location of Judo device
 NAME = "Judo_isoftsaveplus"                     #Name of Judo device
 MANUFACTURER = "ShapeLabs.de"                   #CC BY-NC-SA 4.0
-SW_VERSION = "1.3"
+SW_VERSION = "2.0"
 STATE_UPDATE_INTERVAL = 20                      #Update interval in seconds
 AVAILABILITY_ONLINE = "online"
 AVAILABILITY_OFFLINE = "offline"
 
 #Error- and warning messages of plant published to notification topic ( LOCATION/NAME/notify ). Can be used for hassio telegram bot..
-LANGUAGE = "DE"									# "DE" / "ENG"
-MQTT_DEBUG_LEVEL = 3							# 0=0ff, 1=Judo-Warnings/Errors, 2=Command feedback  3=Script Errors, Exceptions
+LANGUAGE = "DE"									     # "DE" / "ENG"
+MQTT_DEBUG_LEVEL = 2							   # 0=0ff, 1=Judo-Warnings/Errors, 2=Command feedback  3=Script Errors, Exceptions
 MAX_RETRIES = 3
+
+# The maximum slider values can be set for leakage protection can be limited here
+# The limitation can be useful to improve the handling of the sliders in homeassistant
+LIMIT_EXTRACTION_TIME = 60           #can be setup to max 600min
+LIMIT_MAX_WATERFLOW = 3000           #can be setup to max 5000L/h
+LIMIT_EXTRACTION_QUANTITY = 500      #can be setup to max 3000L
+
+#Eviroment Settings
+APPDAEMON = True                     #True, if running in Appdaemon docker directly in HA
 
 # for Appdaemon the whole path is required "/config/appdaemon/apps/temp_getjudo.pkl", otherwise "temp_getjudo.pkl"
 TEMP_FILE = "/config/appdaemon/apps/temp_getjudo.pkl"
-APPDAEMON = True                                #True, if running in Appdaemon docker directly in HA
-
+#TEMP_FILE = "temp_getjudo.pkl"
 
